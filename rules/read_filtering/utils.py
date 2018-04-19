e8b13d7141be31d860ed3ccbb573f547b47f828c
Charles Reid <charlesreid1@gmail.com>
2018-04-18 20:08:19 -0700

    Update all rules - no "serious" parameter validation can be performed if using snakemake because rules and parameters are separated, and we have no knowledge of what rules we are running at validation time.

--- rules/read_filtering/utils.py
+++ rules/read_filtering/utils.py
@@ -4,13 +4,30 @@ def container_image_is_external(biocontainers, app):
     using an external URL (quay.io/biocontainers),
     or is it going to use a local, named Docker image?
     """
-    d = biocontainers[app]
-    if (('use_local' in d) and (d['use_local'] is True)):
-        # This container does not use an external url
-        return False
-    else:
-        # This container uses a quay.io url
-        return True
+    try:
+        d = biocontainers[app]
+        if (('use_local' in d) and (d['use_local'] is True)):
+            # This container does not use an external url
+            return False
+        else:
+            # This container uses a quay.io url
+            return True
+
+    except KeyError:
+        # This is where things get complicated.
+        # Snakemake maintains separation between
+        # parameter validation and knowing what rules
+        # we are actually running.
+        # 
+        # This makes it impossible to validate parameters
+        # except to do it entirely in the dark about what 
+        # we're going to be doing..
+        # 
+        # Solution? 
+        # Ditch parameter validation.
+        # Hope the user knows waht they're doing. 
+        return True 
+
 
 
 def container_image_name(biocontainers, app):
@@ -24,21 +41,24 @@ def container_image_name(biocontainers, app):
     """
     if container_image_is_external(biocontainers,app):
         try:
-            qurl  = biocontainers[k]['quayurl']
-            qvers = biocontainers[k]['version']
-            quayurls.append(qurl + ":" + qvers)
-            return quayurls
+            qurl  = biocontainers[app]['quayurl']
+            qvers = biocontainers[app]['version']
+            return qurl + ":" + qvers
         except KeyError:
-            err = "Error: quay.io URL for %s biocontainer "%(k)
-            err += "could not be determined"
-            raise Exception(err)
+            #err = "Error: quay.io URL for %s biocontainer "%(app)
+            #err += "could not be determined"
+            #raise Exception(err)
+            ## let it ride
+            return ""
 
     else:
         try:
             return biocontainers[app]['local']
         except KeyError:
-            err = "Error: the parameters provided specify a local "
-            err += "container image should be used for %s, but none "%(app)
-            err += "was specified using the 'local' key."
-            raise Exception(err)
+            #err = "Error: the parameters provided specify a local "
+            #err += "container image should be used for %s, but none "%(app)
+            #err += "was specified using the 'local' key."
+            #raise Exception(err)
+            ## let it ride
+            return ""
 
