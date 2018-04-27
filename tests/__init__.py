9ea03d0686dbe173480a7eda3fec9a51791063ed
Charles Reid <charlesreid1@gmail.com>
2018-04-27 12:18:45 -0700

    Remove tests from their old location
    - Tests now live inside the submodule

--- tests/__init__.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-# auto-detect unit tests in the tests/ directory
-# (note: each tests/ dir needs to have an __init__.py to be discoverable)
-def taco_test_suite():
-    test_loader = unittest.TestLoader()
-    test_suite = test_loader.discover('tests', pattern='test_*.py')
-    return test_suite
