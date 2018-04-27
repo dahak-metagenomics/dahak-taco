b426251e948f759f5402c1a32c64a8e4f8eb644f
Charles Reid <charlesreid1@gmail.com>
2018-04-26 20:58:16 -0700

    correct test import statements

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup, find_packages
 import glob
 import os
-import unittest
 
 with open('requirements.txt') as f:
     required = [x for x in f.read().splitlines() if not x.startswith("#")]
@@ -15,21 +14,13 @@ with open('requirements.txt') as f:
 from cli import __version__, _program
 
 
-# auto-detect unit tests in the tests/ directory
-# (note: each tests/ dir needs to have an __init__.py to be discoverable)
-def taco_test_suite():
-    test_loader = unittest.TestLoader()
-    test_suite = test_loader.discover('tests', pattern='test_*.py')
-    return test_suite
-
-
 setup(name=_program,
       version=__version__,
       packages=['cli'],
       package_data={
           '':'*.txt'
       },
-      test_suite='setup.taco_test_suite',
+      test_suite='tests.taco_test_suite',
       description='dahak taco, the command line interface for running dahak workflows',
       url='https://dahak-metagenomics.github.com/dahak-taco',
       author='DIB Lab',
@@ -40,6 +31,7 @@ setup(name=_program,
       {program} = cli.command:main
       """.format(program = _program),
       keywords=[],
-      tests_require=['pytest', 'coveralls'],
+      #tests_require=['pytest', 'coveralls'],
+      tests_require=[],
       zip_safe=False)
 
